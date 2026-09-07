0fa4ea98fa672728c6fc23ebaacb45b19cb9f544
Lucas Rangel <rangellucas2004@gmail.com>
2026-09-07 19:44:56 -0300

    refactor: cambio de estructura en archivos de consultas

--- database/consultas/estudiantes/listar.php
+++ database/consultas/estudiantes/listar.php
@@ -1,14 +1,14 @@
 <?php
     // Traer la conexión de BD
-    include("../../conexion.php");
+    require_once '../conexion.php';
 
     // Preparar consulta
     $consulta = "SELECT nombre_usuario, apellido, email FROM usuarios WHERE id_rol = :rol;";
     $stmt = $phpConn->prepare($consulta);
-    $stmt->execute([':rol' => 3]); // Ejecutar consulta, devolverá un diccionario.
+    $stmt->execute([':rol' => 5]); // Ejecutar consulta, devolverá un diccionario.
     $res = $stmt->fetchAll(PDO::FETCH_ASSOC); // Convierte esto en un array asociativo/diccionario (nombres de las columnas como claves)
     // $res = Array([0] => array(['id'] => 1, ['nombre'] => Juan, ..., etc.), [1] => array(...)...);
-    
+
     // Obtener longitud del array asociativo
     $len = count($res);
 
